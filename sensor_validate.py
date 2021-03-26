d779a5a155a9cb366e668d6d8c4a7e7dfe61f8da
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-03-27 00:43:48 +0530

    Update sensor_validate.py
    Final_checkin_after_resolving_Cyclometric_complexity

--- sensor_validate.py
+++ sensor_validate.py
@@ -4,19 +4,27 @@ def  is_charge_rate_threshold_limit_exceeds(value, nextValue, parameter):
   if nextValue - value > maxDelta[parameter]:
     return True
   return False
+
+
 def validate_charging_parameter_reading(values,parameter):
-   if(is_none(values) == False):
-       last_but_one_reading = len(values) - 1
-       print(last_but_one_reading)
-       print(last_but_one_reading)
+    if(is_none(values) == False): 
+        return parameter_excheed_thresholds_limit(values,parameter)
+    else:
+        print("Given input is None")
+        return False
+    
+
+def parameter_excheed_thresholds_limit(values_1,parameter_1):
+       last_but_one_reading = len(values_1) - 1
        for i in range(last_but_one_reading):
            print(i)
-           if(is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1],parameter)):
-               print("charge rate reached maximum threshold limit exceeds")
+           if(is_charge_rate_threshold_limit_exceeds(values_1[i], values_1[i + 1],parameter_1)):
+               print(f'{parameter_1} maximum threshold limit exceeds')
                return True
-   else:
-       print("Given input is None")
-   return False
+       print("limit not reached")
+       return False
+
+
 def is_none(values):
     if values is None:
         return True
