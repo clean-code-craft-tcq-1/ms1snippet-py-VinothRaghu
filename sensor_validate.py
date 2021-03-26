b8843283500985afc5a71bcd132cd857b99e7f14
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-03-26 21:07:01 +0530

    Update sensor_validate.py
    duplication removal

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,14 +1,12 @@
-def  is_charge_rate_threshold_limit_exceeds(value, nextValue, maxDelta):
-  if nextValue - value > maxDelta:
+maxDelta={"soc":0.05 ,"current":0.1}
+def  is_charge_rate_threshold_limit_exceeds(value, nextValue, parameter):
+  print(maxDelta[parameter])
+  if nextValue - value > maxDelta[parameter]:
     return True
   return False
 def validate_charging_parameter_reading(values,parameter):
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
-	if(parameter=='soc'):
-		if(is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1],0.05)):
-			return True
-	elif(parameter=='current'):
-		if(is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1],0.05)):
-			return True
-	return False
+        if(is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1],parameter)):
+              return True
+        return False
