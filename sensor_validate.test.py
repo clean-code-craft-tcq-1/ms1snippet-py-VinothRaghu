25d76e824f3ac0e3c19ebedafc7f212bd8e23051
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-03-27 01:10:54 +0530

    Update sensor_validate.test.py
    solving indentation error and final checkin

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -2,7 +2,7 @@ import unittest
 import sensor_validate
 
 class SensorValidatorTest(unittest.TestCase):
- def test_reports_error_when_current_jumps(self):
+  def test_reports_error_when_current_jumps(self):
     self.assertTrue(
       sensor_validate.validate_charging_parameter_reading([0.03, 0.03, 0.03, 0.33],"current")
     )
