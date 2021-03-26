78f056f76c5427da1c35484b3a91687ef5881d53
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-03-27 00:05:09 +0530

    Update sensor_validate.py
    None function added

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,12 +1,24 @@
-maxDelta={"soc":0.05 ,"current":0.1}
 def  is_charge_rate_threshold_limit_exceeds(value, nextValue, parameter):
+  maxDelta={"soc":0.05 ,"current":0.1}
   print(maxDelta[parameter])
   if nextValue - value > maxDelta[parameter]:
     return True
   return False
 def validate_charging_parameter_reading(values,parameter):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-        if(is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1],parameter)):
-              return True
+   if(is_none(values) == False):
+       last_but_one_reading = len(values) - 1
+       print(last_but_one_reading)
+       print(last_but_one_reading)
+       for i in range(last_but_one_reading):
+           print(i)
+           if(is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1],parameter)):
+               print("charge rate reached maximum threshold limit exceeds")
+               return True
+   else:
+       print("Given input is None")
+   return False
+def is_none(values):
+    if values is None:
+        return True
+    else:
         return False
