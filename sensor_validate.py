99cc2cfd4f398bafde5375b2dc521a40668f8aba
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-03-26 19:31:37 +0530

    Update sensor_validate.py

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,5 +1,5 @@
 
-def  is_thresholdreached_forchangerate(value, nextValue, maxDelta):
+def  is_threshold_reached_for_change_rate(value, nextValue, maxDelta):
   if nextValue - value > maxDelta:
     return False
   return True
