b7a658f2c3bcda81be0c985f8ff0638f3026a624
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-03-27 00:08:50 +0530

    Update sensor_validate.test.py
    Test case for None added

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -3,13 +3,17 @@ import sensor_validate
 
 class SensorValidatorTest(unittest.TestCase):
   def test_reports_error_when_soc_jumps(self):
-    self.assertFalse(
+    self.assertTrue(
       sensor_validate.validate_charging_parameter_reading([0.0, 0.01, 0.5, 0.51],"soc")
     )
   
   def test_reports_error_when_current_jumps(self):
-    self.assertFalse(
+    self.assertTrue(
       sensor_validate.validate_charging_parameter_reading([0.03, 0.03, 0.03, 0.33],"current")
+    )
+  def test_reports_error_when_none_sent(self):
+    self.assertFalse(
+      sensor_validate.validate_charging_parameter_reading(None)
     )
 
 if __name__ == "__main__":
