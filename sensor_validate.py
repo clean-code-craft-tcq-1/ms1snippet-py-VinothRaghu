008f6bd303b86a2b4d1c16ffd43d1017e13c414b
VinothRaghu <77480559+VinothRaghu@users.noreply.github.com>
2021-03-26 19:53:31 +0530

    Update sensor_validate.py
    function naming changes for better readablity

--- sensor_validate.py
+++ sensor_validate.py
@@ -7,13 +7,13 @@ def  is_charge_rate_threshold_limit_exceeds(value, nextValue, maxDelta):
 def validate_soc_reading(values):
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
-    if(not is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1], 0.05)):
-      return False
-  return True
+    if(is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1], 0.05)):
+      return True
+  return False
 
 def validate_current_reading(values):
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
-    if(not is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1], 0.1)):
-      return False
-  return True
+    if(is_charge_rate_threshold_limit_exceeds(values[i], values[i + 1], 0.1)):
+      return True
+  return False
